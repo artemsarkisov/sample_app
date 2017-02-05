c420dace70cbae90a25459074f2be2e5bc62bb2e
Artem Sarkisov <artem.sarkisov@jetbrains.com>
2017-02-05 22:58:16 +0300

    Added titles for Home, About, Help

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'credit_card/userpage'
+
+  get 'foo/bar'
+
+  get 'foo/baz'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
+
+  get 'static_pages/about'
 
   root 'application#hello'
 end
